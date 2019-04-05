deae19b12be0d703aecbe19128cb5697eb5d7bcc
Štěpán Vraný <stepan@vrany.name>
2019-04-05 15:39:46 +0200

    Update gcr-init.sh

--- gcr-init.sh
+++ gcr-init.sh
@@ -7,11 +7,13 @@
 
 # Check required variables
 [ -z "$BUILD_VAULT_TOKEN" ] && { echo "BUILD_VAULT_TOKEN is required"; exit 1; }
+[ -z "$BUILD_VAULT_ADDR" ] && { echo "BUILD_VAULT_ADDR is required"; exit 1; }
 [ -z "$BUILD_GCLOUD_SA_VAULT_PATH" ] && { echo "BUILD_GCLOUD_SA_VAULT_PATH is required"; exit 1; }
 [ -z "$BUILD_GCLOUD_SA_VAULT_KEY" ] && { echo "BUILD_GCLOUD_SA_VAULT_KEY is required"; exit 1; }
 
 echo "Initializing gcloud auth"
 # set Vault token for authentication
+export VAULT_ADDR="${BUILD_VAULT_ADDR}"
 export VAULT_TOKEN="${BUILD_VAULT_TOKEN}"
 # get Google SA key from Vault
 vault read -format=json -field=data "${BUILD_GCLOUD_SA_VAULT_PATH}" | jq -r ".${BUILD_GCLOUD_SA_VAULT_KEY}" > /tmp/key.json
