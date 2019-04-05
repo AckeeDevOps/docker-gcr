3c81fc1086bf73118c46d628ca36a47c247f4788
Štěpán Vraný <stepan@vrany.name>
2019-04-05 13:57:30 +0200

    Update gcr-init.sh

--- gcr-init.sh
+++ gcr-init.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
 # Required variables:
-#   GCLOUD_SA_KEY: base64 encoded string with service account key (cat /pat/to/key | base64 -w0)
+#   BUILD_GCLOUD_SA_VAULT_PATH: Vault path. e.g. secret/data/projects/bla/bla
+#   BUILD_GCLOUD_SA_VAULT_KEY: name of field with SA key e.g. SA_KEY, value should contain JSON formated SA key
+#   BUILD_VAULT_TOKEN: Vault token in plain text
 
 # Check required variables
-[ -z "$GCLOUD_SA_KEY" ] && { echo "GCLOUD_SA_KEY is required"; exit 1; }
+[ -z "$BUILD_VAULT_TOKEN" ] && { echo "BUILD_VAULT_TOKEN is required"; exit 1; }
+[ -z "$BUILD_GCLOUD_SA_VAULT_PATH" ] && { echo "BUILD_GCLOUD_SA_VAULT_PATH is required"; exit 1; }
+[ -z "$BUILD_GCLOUD_SA_VAULT_KEY" ] && { echo "BUILD_GCLOUD_SA_VAULT_KEY is required"; exit 1; }
 
 echo "Initializing gcloud auth"
-echo $GCLOUD_SA_KEY | base64 -d > /tmp/key.json
+# set Vault token for authentication
+export VAULT_TOKEN="${BUILD_VAULT_TOKEN}"
+# get Google SA key from Vault
+vault read -format=json -field=data "${BUILD_GCLOUD_SA_VAULT_PATH}" | jq -r ".${BUILD_GCLOUD_SA_VAULT_KEY}" > /tmp/key.json
+# activate service account
 gcloud auth activate-service-account --key-file=/tmp/key.json
 
 echo "Initializing Docker"
